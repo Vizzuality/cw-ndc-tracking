0f99ee68783d88528faa66001612abf91b4e9e96
Simao Belchior <simaobelchior@gmail.com>
2018-04-17 19:41:40 +0100

    Add devise to User model

--- db/migrate/20180417183819_create_users.rb
+++ db/migrate/20180417183819_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :email, null: false, default: ""
       t.boolean :is_admin
 
       t.timestamps
