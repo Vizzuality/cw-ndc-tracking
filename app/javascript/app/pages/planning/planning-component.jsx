de6ce84f3debe9e0baa06a95e332de72b7b20249
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-04-18 17:39:10 +0200

    Target first draft

--- app/javascript/app/pages/planning/planning-component.jsx
+++ app/javascript/app/pages/planning/planning-component.jsx
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react';
+import Target from 'components/target';
 import styles from './planning-styles.scss';
 
 class Planning extends PureComponent {
   render() {
     return (
       <div className={styles.page}>
-        <h2>Planning page</h2>
+        <Target
+          title={'GHG target'}
+          summary={
+            'Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025.'
+          }
+          reportedPercentage={77}
+        />
+        <Target
+          title={'GHG target'}
+          summary={
+            'Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025.'
+          }
+          reportedPercentage={100}
+        />
       </div>
     );
   }
