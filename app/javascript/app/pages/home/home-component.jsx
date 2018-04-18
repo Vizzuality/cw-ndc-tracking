2c24b7208944506f0799fd8a9c9187c752e4f4f1
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-04-18 14:30:22 +0200

    Add sample indicators to home page

--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import Search from 'components/search';
-import Dropdown from 'components/dropdown';
+import Indicator from 'components/indicator';
 import styles from './home-styles.scss';
 
 class Home extends PureComponent {
@@ -16,15 +16,28 @@ class Home extends PureComponent {
           className={styles.search}
           autofocus
         />
-        <Dropdown
-          placeholder="selection"
-          options={[
-            { label: 'value 1', value: 'value 1' },
-            { label: 'value 2', value: 'value 2' }
-          ]}
-          label="select some stuff"
-          disabled
-        />
+        <div>
+          <Indicator
+            title="titleee"
+            fieldType="textarea"
+            inputUnit="tons"
+            label="ghg emissions"
+          />
+          <Indicator
+            title="Conditional upon international provision of means of implementation: capacity building, technology development and transfer, financing"
+            fieldType="dropdown"
+            dropdownOptions={[
+              { label: 'No', value: 'No' },
+              { label: 'Yes', value: 'Yes' }
+            ]}
+          />
+          <Indicator
+            title="Emissions level in base year"
+            fieldType="number"
+            inputUnit="MtCO2eq"
+            label="Value"
+          />
+        </div>
       </div>
     );
   }
