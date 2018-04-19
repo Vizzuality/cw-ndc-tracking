6f40699c59af3899425f731c9bd2a0cff80762be
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-04-19 13:43:53 +0200

    Add children and update styles

--- app/javascript/app/components/planning-target/planning-target-component.jsx
+++ app/javascript/app/components/planning-target/planning-target-component.jsx
@@ -1,41 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { themr } from 'react-css-themr';
-import Button from 'components/button';
-import Icon from 'components/icon';
-import ProgressBar from 'components/progress-bar';
-import deleteIcon from 'assets/icons/info.svg';
 
-import squareButtonTheme from 'styles/themes/button/button-square.scss';
-import yellowButtonTheme from 'styles/themes/button/button-yellow.scss';
-import blueIconTheme from 'styles/themes/icon/icon-blue.scss';
 import styles from './planning-target-styles.scss';
 
-const Target = ({ title, theme, summary, reportedPercentage }) => (
-  <div className={theme.wrapper}>
-    <div className={theme.infoContainer}>
-      <p className={theme.name}>{title}</p>
-      {summary && <p className={theme.summary}>{summary}</p>}
-    </div>
-    {reportedPercentage === 100 ? (
-      <div className={theme.buttonContainer}>
-        <Button theme={squareButtonTheme} onClick={() => true}>
-          <Icon theme={blueIconTheme} icon={deleteIcon} />
-        </Button>
+const Target = ({ title, theme, summary, children }) => (
+  <div>
+    <div className={theme.wrapper}>
+      <div className={theme.infoContainer}>
+        <p className={theme.name}>{title}</p>
+        {summary && <p className={theme.summary}>{summary}</p>}
       </div>
-    ) : (
-      <Fragment>
-        <ProgressBar progress={reportedPercentage} />
-        <Button theme={yellowButtonTheme} onClick={() => true}>
-          Report
-        </Button>
-      </Fragment>
-    )}
+      <div className={theme.buttonContainer}>{children}</div>
+    </div>
   </div>
 );
 
 Target.propTypes = {
-  reportedPercentage: PropTypes.number,
+  children: PropTypes.node,
   theme: PropTypes.object,
   title: PropTypes.string,
   summary: PropTypes.string
