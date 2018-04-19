b457e6e1f2ef76dfb13c599bda077ebbab140099
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-04-19 11:02:40 +0200

    Add inside elements as children

--- app/javascript/app/components/indicator/indicator-component.jsx
+++ app/javascript/app/components/indicator/indicator-component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { themr } from 'react-css-themr';
 import cx from 'classnames';
-import Input from 'components/input';
-import Dropdown from 'components/dropdown';
+// import Input from 'components/input';
+// import Dropdown from 'components/dropdown';
 import Button from 'components/button';
 import Icon from 'components/icon';
 import deleteIcon from 'assets/icons/info.svg';
@@ -12,27 +12,16 @@ import squareButtonTheme from 'styles/themes/button/button-square.scss';
 import blueIconTheme from 'styles/themes/icon/icon-blue.scss';
 import styles from './indicator-styles.scss';
 
-const Indicator = ({
-  title,
-  fieldType,
-  theme,
-  inputUnit,
-  label,
-  dropdownOptions
-}) => (
+const Indicator = ({ title, fieldType, theme, children }) => (
   <div
     className={cx(theme.wrapper, {
       [theme.textArea]: fieldType === 'textarea'
     })}
   >
     <p className={theme.indicatorTitle}>{title}</p>
-    {fieldType === 'dropdown' ? (
-      dropdownOptions && (
-        <Dropdown options={dropdownOptions} unit={inputUnit} label={label} />
-      )
-    ) : (
-      <Input inputType={fieldType} unit={inputUnit} label={label} />
-    )}
+    <div className="layout-grid-item">
+      <div className={theme.childrenContainer}>{children}</div>
+    </div>
     <div className={theme.buttonContainer}>
       <Button theme={squareButtonTheme} onClick={() => true}>
         <Icon theme={blueIconTheme} icon={deleteIcon} />
@@ -42,10 +31,8 @@ const Indicator = ({
 );
 
 Indicator.propTypes = {
-  dropdownOptions: PropTypes.array,
+  children: PropTypes.node,
   fieldType: PropTypes.string,
-  inputUnit: PropTypes.string,
-  label: PropTypes.string,
   theme: PropTypes.object,
   title: PropTypes.string
 };
--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import Search from 'components/search';
 import Indicator from 'components/indicator';
+import Input from 'components/input';
+import Dropdown from 'components/dropdown';
 import styles from './home-styles.scss';
 
 class Home extends PureComponent {
@@ -17,26 +19,49 @@ class Home extends PureComponent {
           autofocus
         />
         <div>
-          <Indicator
-            title="titleee"
-            fieldType="textarea"
-            inputUnit="tons"
-            label="ghg emissions"
-          />
+          <Indicator title="titleee" fieldType="textarea">
+            <Input
+              label="Label for text area"
+              unit="CO2"
+              inputType="textarea"
+              placeholder="Text Area"
+              onChange={value => value}
+              onBlur={value => value}
+            />
+          </Indicator>
           <Indicator
             title="Conditional upon international provision of means of implementation: capacity building, technology development and transfer, financing"
             fieldType="dropdown"
-            dropdownOptions={[
-              { label: 'No', value: 'No' },
-              { label: 'Yes', value: 'Yes' }
-            ]}
-          />
+          >
+            <Dropdown
+              options={[
+                { label: 'No', value: 'No' },
+                { label: 'Yes', value: 'Yes' }
+              ]}
+              label="ghg emissions"
+            />
+            <Dropdown
+              options={[
+                { label: 'No', value: 'No' },
+                { label: 'Yes', value: 'Yes' }
+              ]}
+              label="ghg emissions"
+            />
+          </Indicator>
           <Indicator
             title="Emissions level in base year"
             fieldType="number"
             inputUnit="MtCO2eq"
             label="Value"
-          />
+          >
+            <Dropdown
+              options={[
+                { label: 'No', value: 'No' },
+                { label: 'Yes', value: 'Yes' }
+              ]}
+              label="ghg emissions"
+            />
+          </Indicator>
         </div>
       </div>
     );
