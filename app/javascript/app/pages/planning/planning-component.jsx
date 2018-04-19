564053f96011f3f6d10c48dd3d0e571ec21e5213
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-04-19 17:12:53 +0200

    Update planning component

--- app/javascript/app/pages/planning/planning-component.jsx
+++ app/javascript/app/pages/planning/planning-component.jsx
@@ -1,14 +1,10 @@
 import React, { PureComponent } from 'react';
-import PlanningTarget from 'components/planning-target';
+import ReactTooltip from 'react-tooltip';
+import Target from 'components/target';
 import Header from 'components/header';
 import PropTypes from 'prop-types';
-import Button from 'components/button';
-import Icon from 'components/icon';
-import deleteIcon from 'assets/icons/info.svg';
 
-import squareButtonTheme from 'styles/themes/button/button-square.scss';
-import blueIconTheme from 'styles/themes/icon/icon-blue.scss';
-
+import tooltipTheme from 'styles/themes/tooltip/default.scss';
 import styles from './planning-styles.scss';
 
 class Planning extends PureComponent {
@@ -18,29 +14,21 @@ class Planning extends PureComponent {
       <div className={styles.page}>
         <Header title="Planning" routes={routes} />
         <div className={styles.targetsContainer}>
-          <PlanningTarget
+          <Target
             title={'GHG target'}
             summary={
               'Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025.'
             }
-          >
-            <Button theme={squareButtonTheme} onClick={() => true}>
-              <Icon theme={blueIconTheme} icon={deleteIcon} />
-            </Button>
-          </PlanningTarget>
-          <PlanningTarget
+            hasRemoveAction
+            hasEditAction
+            infoText="some info text"
+          />
+          <Target
             title={'GHG target'}
-            summary={
-              'Brazil intends to commit to reduce greenhouse gas emissions by 37% below 2005 levels in 2025.'
-            }
-          >
-            <Button theme={squareButtonTheme} onClick={() => true}>
-              <Icon theme={blueIconTheme} icon={deleteIcon} />
-            </Button>
-            <Button theme={squareButtonTheme} onClick={() => true}>
-              <Icon theme={blueIconTheme} icon={deleteIcon} />
-            </Button>
-          </PlanningTarget>
+            hasEditAction
+            infoText="some other info text"
+          />
+          <ReactTooltip className={tooltipTheme.tooltip} />
         </div>
       </div>
     );
