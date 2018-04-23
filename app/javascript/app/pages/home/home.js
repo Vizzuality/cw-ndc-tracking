469a944bff733fca6307c2882dccfab864196ea9
Javi Abia <javiabia@eriks-Air.home>
2018-04-23 17:14:03 +0200

    Remove home component

--- app/javascript/app/pages/home/home.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-
-import HomeComponent from './home-component';
-
-const mapStateToProps = state => state;
-
-export default connect(mapStateToProps, null)(HomeComponent);
