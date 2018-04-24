32a1fe1f2c700088ea8b9a4a85a015fbd0db55dc
Javi Abia <javiabia@eriks-Air.home>
2018-04-24 16:30:35 +0200

    Extract logic out of mapState on planning and tracking containers

--- app/javascript/app/pages/planning/planning.js
+++ app/javascript/app/pages/planning/planning.js
@@ -4,19 +4,25 @@ import PlanningComponent from './planning-component';
 
 const hasSections = sections => Object.keys(sections).length > 0;
 
-const mapStateToProps = ({ location, sections }) => ({
-  routes: Object.values(location.routesMap).filter(r => !!r.nav),
-  selectedCategory: hasSections(sections)
-    ? sections.find(section => section.slug === 'planning').categories[0].slug
-    : null,
-  categories: hasSections(sections)
+const getCategories = sections =>
+  (hasSections(sections)
     ? sections
       .find(section => section.slug === 'planning')
       .categories.map(category => ({
         ...category,
         path: `/planning/${category.slug}`
       }))
-    : []
+    : []);
+
+const getSelectedCategory = sections =>
+  (hasSections(sections)
+    ? sections.find(section => section.slug === 'planning').categories[0].slug
+    : null);
+
+const mapStateToProps = ({ location, sections }) => ({
+  routes: Object.values(location.routesMap).filter(r => !!r.nav),
+  selectedCategory: getSelectedCategory(sections),
+  categories: getCategories(sections)
 });
 
 export default connect(mapStateToProps, null)(PlanningComponent);
