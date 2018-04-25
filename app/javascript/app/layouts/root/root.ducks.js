dd56bd35b78f205badba17d17f381cd0c7ed7236
Javi Abia <javiabia@eriks-Air.home>
2018-04-25 12:28:10 +0200

    Fetch only if sections is empty

--- app/javascript/app/layouts/root/root.ducks.js
+++ app/javascript/app/layouts/root/root.ducks.js
@@ -1,13 +1,19 @@
 const { DEV_API } = process.env;
 
-export async function getSectionsThunk(dispatch) {
-  const sections = await fetch(
-    `${DEV_API}/sections?includes[]=categories&includes[]=targets`
-  ).then(function (response) {
-    if (response.ok) return response.json();
-    throw Error(response.statusText);
-  });
-  dispatch({ type: 'GET_SECTIONS', payload: sections });
+export async function getSectionsThunk(dispatch, getState) {
+  const { sections } = getState();
+
+  if (sections.length === 0) {
+    const apiSections = await fetch(
+      `${DEV_API}/sections?includes[]=categories&includes[]=targets`
+    ).then(function (response) {
+      if (response.ok) return response.json();
+      throw Error(response.statusText);
+    });
+    dispatch({ type: 'GET_SECTIONS', payload: apiSections });
+  } else {
+    dispatch({ type: 'GET_SECTIONS', payload: sections });
+  }
 }
 
 export default (state = [], action) =>
