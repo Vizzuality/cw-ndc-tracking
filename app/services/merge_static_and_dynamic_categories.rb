ef4cd71fdc9dfd5974caaff96b8014108f595907
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-04-27 14:06:34 +0100

    More consistent variable names

--- app/services/merge_static_and_dynamic_categories.rb
+++ app/services/merge_static_and_dynamic_categories.rb
@@ -1,19 +1,19 @@
 class MergeStaticAndDynamicCategories
-  # @param section [Static::Section]
+  # @param static_section [Static::Section]
   # @param static_categories [Array<Static::Category>]
-  # @param report_categories [Array<Category>]
-  def initialize(section, static_categories, report_categories)
-    @section = section
+  # @param dynamic_categories [Array<Category>]
+  def initialize(static_section, static_categories, dynamic_categories)
+    @static_section = static_section
     @static_categories = static_categories
-    @report_categories = report_categories
+    @dynamic_categories = dynamic_categories
   end
 
   # @param year [Integer]
   # @param category_includes [Array<Symbol>]
   def call(year, category_includes)
     @static_categories.map do |static_category|
-      match = @report_categories.detect do |report_category|
-        report_category.section_slug == @section.slug &&
+      match = @dynamic_categories.detect do |report_category|
+        report_category.section_slug == @static_section.slug &&
           report_category.slug == static_category.slug
       end
       category_hash = static_category.to_hash
