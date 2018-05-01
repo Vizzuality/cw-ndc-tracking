6a06065a687c5030480b544332b6666fab2e6444
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-05-01 13:34:19 +0100

    Variable renaming

--- app/services/get_all_indicators.rb
+++ app/services/get_all_indicators.rb
@@ -1,12 +1,12 @@
 class GetAllIndicators
   # @param report [Report]
   # @param static_section [Static::Section]
-  # @param category [Static::Category]
+  # @param static_category [Static::Category]
   # @param static_target [Static::Target]
-  def initialize(report, static_section, category, static_target)
+  def initialize(report, static_section, static_category, static_target)
     @report = report
     @static_section = static_section
-    @static_category = category
+    @static_category = static_category
     @category = @report.categories.where(
       section_slug: @static_section.slug, slug: @static_category.slug
     ).first
