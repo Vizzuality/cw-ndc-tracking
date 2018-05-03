238e5ac3bd93b078b2a5a8bb7e01b3e61ce702fd
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-05-03 13:42:51 +0200

    Add reporting route

--- app/javascript/app/layouts/root/root.js
+++ app/javascript/app/layouts/root/root.js
@@ -5,10 +5,16 @@ import Component from './root-component';
 
 const mapStateToProps = ({ location, sections }) => ({
   route: location.routesMap[location.type],
-  sections: sections.map(section => ({
-    ...section,
-    path: `/${section.slug}/${DEFAULT_TARGET}`
-  }))
+  sections: sections
+    .map(section => ({
+      ...section,
+      path: `/${section.slug}/${DEFAULT_TARGET}`
+    }))
+    .concat({
+      title: 'Reporting',
+      slug: 'reporting',
+      path: '/reporting'
+    })
 });
 
 export default connect(mapStateToProps, null)(Component);
