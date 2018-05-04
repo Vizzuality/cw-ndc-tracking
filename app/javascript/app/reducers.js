41a0ab6d3422c489ae0a43c582c69a4247d1dc61
Javi Abia <javiabia@eriks-Air.home>
2018-05-04 17:40:01 +0200

    Add api action creator

--- app/javascript/app/reducers.js
+++ app/javascript/app/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 // Router
 import router from './router';
-import sections from './providers/sections.providers';
+import sections from './providers/sections.provider';
 import { setUser } from './pages/login/login-reducers';
 
 export default combineReducers({
--- app/javascript/app/services/api.service.js
+++ app/javascript/app/services/api.service.js
@@ -1,12 +1,15 @@
 const { API_URL } = process.env;
 
-export const apiGet = (endpoint, getState) => {
+export const get = 'GET';
+export const patch = 'PATCH';
+
+export const apiRequest = (endpoint, method, getState) => {
   const token =
     getState().user.authentication_token || localStorage.getItem('CWTTT');
   const email = getState().user.email || localStorage.getItem('user');
 
   const configWithHeaders = {
-    method: 'GET',
+    method,
     headers: new Headers({
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -16,3 +19,10 @@ export const apiGet = (endpoint, getState) => {
   };
   return fetch(`${API_URL}/${endpoint}`, configWithHeaders);
 };
+
+export const apiActionCreator = (path, method, successAction) => ({
+  type: 'API',
+  path,
+  method,
+  onSuccess: successAction
+});
