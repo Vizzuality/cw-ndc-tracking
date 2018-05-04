661cc4bb76f844ff0049c9e5f04969e88c78e15e
Alvaro Leal <aleal@icometrix.com>
2018-05-04 13:15:09 +0200

    Add patch to indicators

--- app/javascript/app/pages/edit-target/edit-target.js
+++ app/javascript/app/pages/edit-target/edit-target.js
@@ -3,25 +3,36 @@ import { connect } from 'react-redux';
 import { history } from 'redux-first-router';
 import { updateUrlParam } from 'utils/navigation';
 import qs from 'query-string';
+import PropTypes from 'prop-types';
+import { patchIndicator } from 'providers/indicators.providers';
 import { getTarget, getIndicators } from './edit-target-selectors';
 import Component from './edit-target-component';
 
 const mapStateToProps = ({ location, sections }) => {
   const query = qs.parse(history().location.search);
+  const section = location.pathname.split('/')[1];
+  const category = location.payload.category;
   const state = {
     sections,
-    section: location.pathname.split('/')[1],
-    category: location.payload.category,
+    section,
+    category,
     target: location.payload.target,
     search: (query && query.search) || null
   };
 
   return {
+    section,
+    category,
     target: getTarget(state),
     indicators: getIndicators(state),
     search: state.search
   };
 };
+
+const mapDispatchToProps = dispatch => ({
+  handlePatchIndicator: (urlParams, valueParams) =>
+    dispatch(patchIndicator(urlParams, valueParams))
+});
 
 class EditTargetContainer extends PureComponent {
   render() {
@@ -29,11 +40,29 @@ class EditTargetContainer extends PureComponent {
       updateUrlParam({ name: 'search', value: query });
     };
 
+    const handleEditIndicator = (indicatorSlug, valueLabel, value) => {
+      const { section, category, target, handlePatchIndicator } = this.props;
+      return handlePatchIndicator(
+        { section, category, target: target.slug, indicator: indicatorSlug },
+        { valueLabel, value }
+      );
+    };
+
     return createElement(Component, {
       ...this.props,
-      handleOnSearch
+      handleOnSearch,
+      handleEditIndicator
     });
   }
 }
 
-export default connect(mapStateToProps, null)(EditTargetContainer);
+EditTargetContainer.propTypes = {
+  section: PropTypes.string,
+  category: PropTypes.string,
+  target: PropTypes.object,
+  handlePatchIndicator: PropTypes.func
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  EditTargetContainer
+);
--- app/javascript/app/services/api.service.js
+++ app/javascript/app/services/api.service.js
@@ -1,18 +1,30 @@
 const { API_URL } = process.env;
 
-export const apiGet = (endpoint, getState) => {
-  const token =
-    getState().user.authentication_token || localStorage.getItem('CWTTT');
-  const email = getState().user.email || localStorage.getItem('user');
+const getHeaders = (getState, localStorage) =>
+  new Headers({
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'X-User-Email': getEmail(getState, localStorage),
+    'X-User-Token': getToken(getState, localStorage)
+  });
+const getToken = (getState, localStorage) =>
+  getState().user.authentication_token || localStorage.getItem('CWTTT');
+const getEmail = (getState, localStorage) =>
+  getState().user.email || localStorage.getItem('user');
 
+export const apiGet = (endpoint, getState) => {
   const configWithHeaders = {
     method: 'GET',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'X-User-Email': email,
-      'X-User-Token': token
-    })
+    headers: getHeaders(getState, localStorage)
+  };
+  return fetch(`${API_URL}/${endpoint}`, configWithHeaders);
+};
+
+export const apiPatch = (endpoint, getState, body) => {
+  const configWithHeaders = {
+    method: 'PATCH',
+    body,
+    headers: getHeaders(getState, localStorage)
   };
   return fetch(`${API_URL}/${endpoint}`, configWithHeaders);
 };
