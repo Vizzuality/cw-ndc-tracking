5e9625d858e044851ae529713398bf33e876e780
Javi Abia <javiabia@eriks-Air.home>
2018-05-06 22:22:49 +0200

    Add categories, targets and indicators reducers

--- app/javascript/app/reducers.js
+++ app/javascript/app/reducers.js
@@ -3,10 +3,16 @@ import { combineReducers } from 'redux';
 // Router
 import router from './router';
 import sections from './providers/sections.provider';
+import categories from './providers/categories.provider';
+import targets from './providers/targets.provider';
+import indicators from './providers/indicators.provider';
 import { setUser } from './pages/login/login-reducers';
 
 export default combineReducers({
   location: router.reducer,
   sections,
+  categories,
+  targets,
+  indicators,
   user: setUser
 });
