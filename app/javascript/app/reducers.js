efbd79e25eb0084465d8e57be37027abb2d722d1
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-05-10 09:58:52 +0200

    Add planning, tracking and report reducers

--- app/javascript/app/reducers.js
+++ app/javascript/app/reducers.js
@@ -3,16 +3,16 @@ import { combineReducers } from 'redux';
 // Router
 import router from './router';
 import sections from './providers/sections.provider';
-import categories from './providers/categories.provider';
-import targets from './providers/targets.provider';
-import indicators from './providers/indicators.provider';
+import tracking from './providers/tracking.provider';
+import planning from './providers/planning.provider';
+import report from './providers/report.provider';
 import { setUser } from './pages/login/login-reducers';
 
 export default combineReducers({
   location: router.reducer,
   sections,
-  categories,
-  targets,
-  indicators,
+  tracking,
+  planning,
+  report,
   user: setUser
 });
