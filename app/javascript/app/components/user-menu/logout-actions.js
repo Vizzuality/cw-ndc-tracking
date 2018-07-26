cd9f4efebd01f635d9f15e7b7c8049713f9906da
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-07-26 17:50:46 +0200

    Fix logout

--- app/javascript/app/components/user-menu/logout-actions.js
+++ app/javascript/app/components/user-menu/logout-actions.js
@@ -1,4 +1,6 @@
 import { LOGIN } from 'router';
+import { createAction } from 'redux-actions';
 
 // Action creators
+export const setUser = createAction('SET_USER');
 export const navigateToLogin = () => ({ type: LOGIN });
--- app/javascript/app/components/user-menu/user-menu.js
+++ app/javascript/app/components/user-menu/user-menu.js
@@ -1,19 +1,57 @@
+import { PureComponent, createElement } from 'react';
 import { connect } from 'react-redux';
 import { SETTINGS } from 'app/router';
+import { logout } from 'services/login.service';
+import { PropTypes } from 'prop-types';
+import * as actions from './logout-actions';
 import Component from './user-menu-component';
 
-const mapStateToProps = () => {
-  const userMenuOptions = [
-    {
-      label: 'SETTINGS',
-      path: { type: SETTINGS }
-    },
-    {
-      label: 'LOGOUT',
-      action: null
-    }
-  ];
-  return { userMenuOptions };
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = dispatch => ({
+  dispatch,
+  ...actions
+});
+
+class UserMenuContainer extends PureComponent {
+  constructor() {
+    super();
+    this.userMenuOptions = [
+      {
+        label: 'SETTINGS',
+        path: { type: SETTINGS }
+      },
+      {
+        label: 'LOGOUT',
+        action: this.handleLoginThunk.bind(this)
+      }
+    ];
+  }
+
+  handleLoginThunk() {
+    const { dispatch, setUser, navigateToLogin } = this.props;
+    logout().then(ok => {
+      if (ok) {
+        dispatch(setUser({}));
+        localStorage.setItem('CWTTT', '');
+        localStorage.setItem('user', '');
+        dispatch(navigateToLogin());
+      }
+    });
+  }
+
+  render() {
+    return createElement(Component, {
+      ...this.props,
+      userMenuOptions: this.userMenuOptions
+    });
+  }
+}
+
+UserMenuContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  setUser: PropTypes.func.isRequired,
+  navigateToLogin: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps, null)(Component);
+export default connect(mapStateToProps, mapDispatchToProps)(UserMenuContainer);
