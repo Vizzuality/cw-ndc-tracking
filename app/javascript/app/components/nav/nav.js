14a6d80b1b3a3d84047053f7de0fbeef60b31b8e
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-07-31 11:13:35 +0200

    Hide nav lost in conflicts

--- app/javascript/app/components/nav/nav.js
+++ app/javascript/app/components/nav/nav.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import Component from './nav-component';
 
-const mapStateToProps = ({ location }) => ({ location });
+const mapStateToProps = ({ location }) => ({
+  hideNav: location && location.routesMap[location.type].noNav
+});
 
 export default connect(mapStateToProps, null)(Component);
