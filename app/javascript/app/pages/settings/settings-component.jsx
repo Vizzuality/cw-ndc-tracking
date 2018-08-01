e2fa55c6287a894acc0074270a2606472bd95199
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-08-01 20:06:26 +0200

    Extract function

--- app/javascript/app/pages/settings/settings-component.jsx
+++ app/javascript/app/pages/settings/settings-component.jsx
@@ -6,8 +6,19 @@ import styles from './settings-styles.scss';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Settings extends PureComponent {
-  render() {
+  renderUser() {
     const { user } = this.props;
+    return (
+      user && (
+        <div>
+          <div>{`${user.first_name} ${user.last_name}`}</div>
+          <div>{user.email}</div>
+        </div>
+      )
+    );
+  }
+
+  render() {
     return (
       <div className={styles.page}>
         <Header title="Settings" className={styles.header} />
@@ -16,12 +27,7 @@ class Settings extends PureComponent {
             title={'Personal info'}
             editActionLink={'/settings/info'}
           >
-            {user && (
-              <div>
-                <div>{`${user.first_name} ${user.last_name}`}</div>
-                <div>{user.email}</div>
-              </div>
-            )}
+            {this.renderUser()}
           </SettingsItem>
           <SettingsItem
             title={'Password'}
